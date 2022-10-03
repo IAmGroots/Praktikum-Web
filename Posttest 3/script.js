f473a57833edd63c5f1cfd08dda688d88cdf8710
IAmGroots <hadiseptian.crb@gmail.com>
2022-10-03 23:45:02 +0800

    icon mode

--- Posttest 3/script.js
+++ Posttest 3/script.js
@@ -2,15 +2,19 @@
 // Light Mode or Dark Mode
 function lightMode(){
   const isLight = document.body.classList.toggle("light-theme")
+  let icon = document.getElementById("icon-mode");
   let mode = ""
   if (isLight){
     mode = "Light Mode"
+    icon = "images/sun1.png";
   } else {
     mode = "Dark Mode"
+    icon = "images/moon.png";
   }
 
   // Manipulasi DOM 1
   document.getElementById("change-mode").innerHTML = mode
+  document.getElementById("icon-mode").src = icon
 }
 
 // Pop Up
